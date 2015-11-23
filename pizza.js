db9d778b7750edbc3ee842a175931d60a9016514
Giancarlo <guerragiancarlo8@gmail.com>
2015-11-23 14:43:14 +0100

    added active button toggle

--- pizza.js
+++ pizza.js
@@ -1,22 +1,28 @@
 // Write your Pizza Builder JavaScript in this file.
 
 
-$('.btn-pepperonni').on('click', function() {
+$('.btn-pepperonni').on('click', function(event) {
 	$('.pep').toggle();
+	$(event.currentTarget).toggleClass('active')
 });
 
-$('.btn-mushrooms').on('click', function() {
+$('.btn-mushrooms').on('click', function(event) {
 	$('.mushroom').toggle();
+	$(event.currentTarget).toggleClass('active')
 });
 
-$('.btn-green-peppers').on('click', function() {
+$('.btn-green-peppers').on('click', function(event) {
 	$('.green-pepper').toggle();
+	$(event.currentTarget).toggleClass('active')
 });
 
-$('.btn-sauce').on('click',function(){
+$('.btn-sauce').on('click',function(event){
   $('.sauce').toggleClass('sauce-white');
+  $(event.currentTarget).toggleClass('active')
 });
 
-$('.btn-crust').on('click',function(){
+$('.btn-crust').on('click',function(event){
   $('.crust').toggleClass('crust-gluten-free');
+  $(event.currentTarget).toggleClass('active')
 });
+
