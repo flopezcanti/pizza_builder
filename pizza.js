257db158fa9a692891e9dc72f7b1cc497cb95e8b
Fernando López-Canti <flopezcanti@gmail.com>
2015-11-23 14:27:28 +0100

    First iteration

--- pizza.js
+++ pizza.js
@@ -1,1 +1,14 @@
 // Write your Pizza Builder JavaScript in this file.
+
+
+$('.btn-pepperonni').on('click', function() {
+	$('.pep').toggle()
+});
+
+$('.btn-mushrooms').on('click', function() {
+	$('.mushroom').toggle()
+});
+
+$('.btn-green-peppers').on('click', function() {
+	$('.green-pepper').toggle()
+});
